a2b62578d912720def833a91948c11ccc3a218b0
Dewaley <adewaleademilua@gmail.com>
2022-03-05 22:43:40 +0100

    first api test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
+import Input from './components/Input';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState({});
+  const search = async (e) => {
+    e.preventDefault();
+    const key = 'bd9ce425aa545bbb06b29e8abcd77fa6';
+    const url = 'https://api.openweathermap.org/data/2.5/';
+    const res = await fetch(`${url}weather?q=${query}&appid=${key}`);
+    const response = await res.json();
+    setResult(response);
+    console.log(response);
+    setQuery('');
+  };
   return (
-    <div className="App">
-      <h1 className='text-6xl'>Hello World</h1>
+    <div className='flex items-center max-h-screen h-screen w-screen max-w-screen overflow-hidden bg-sky-900 flex-col'>
+      <Input query={query} setQuery={setQuery} search={search} />
+      {typeof result.main !== undefined ? (
+        <div>
+          <h1>
+            {result.name}, {result.sys.country} 
+          </h1>
+        </div>
+      ) : (
+        <h1>Hello</h1>
+      )}
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Input = ({query,setQuery,search}) => {
+  return (
+    <div className='my-5 max-w-sm'>
+     <form action="" onSubmit={search} className='border-b-white border-2 border-transparent flex justify-center p-2 text-white w-full'>
+      <input type="text" name="" id="" onChange={(e)=>setQuery(e.target.value)} value={query} className='bg-transparent outline-none w-full' placeholder='Enter location'/>
+      <button type="submit" className='bg-white p-1 text-sky-900 rounded-md'>Search</button>
+     </form>
+    </div>
+  )
+}
+
+export default Input
